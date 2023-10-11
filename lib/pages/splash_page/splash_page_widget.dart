9258aabba541818cd18825c864f7f174672817d2
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-11 06:40:56 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/splash_page/splash_page_widget.dart
+++ lib/pages/splash_page/splash_page_widget.dart
@@ -97,6 +97,29 @@ class _SplashPageWidgetState extends State<SplashPageWidget> {
                   borderRadius: BorderRadius.circular(8.0),
                 ),
               ),
+              FFButtonWidget(
+                onPressed: () {
+                  print('Button pressed ...');
+                },
+                text: 'Button',
+                options: FFButtonOptions(
+                  height: 40.0,
+                  padding: EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+                  iconPadding:
+                      EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                  color: FlutterFlowTheme.of(context).primary,
+                  textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                        fontFamily: 'Readex Pro',
+                        color: Colors.white,
+                      ),
+                  elevation: 3.0,
+                  borderSide: BorderSide(
+                    color: Colors.transparent,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
             ],
           ),
         ),
