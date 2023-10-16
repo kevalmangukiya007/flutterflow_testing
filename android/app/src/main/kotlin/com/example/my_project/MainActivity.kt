508ee799b8b93dbe7750275db299161c92e6d37f
kevalmangukiya007 <130359399+kevalmangukiya007@users.noreply.github.com>
2023-10-16 11:54:41 +0530

    update

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.findgift
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/index.dart
+++
@@ -1,2 +0,0 @@
-// Export pages
-export '/pages/splash_page/splash_page_widget.dart' show SplashPageWidget;
